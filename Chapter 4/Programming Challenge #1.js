7cb3cd54bd3d25c28a317e3897a87c34f4716fd1
c3rb1n <ilya_prudnikov97@mail.ru>
2020-07-06 06:36:19 +0300

    add solutions for chapter 4

---
+++ Chapter 4/Programming Challenge #1.js
@@ -0,0 +1,26 @@
+// Make object
+let scores = {
+	artyom: 0,
+	viktor: 15,
+	egor: 3,
+	artur: 10,
+	boris: 5
+};
+
+// Increase Artyom's scores by 10
+scores.artyom += 10;
+
+// Increase Viktor's scores by 20
+scores.viktor += 20;
+
+// Increase Egor's scores by 30
+scores.egor += 30;
+
+// Increase Artur's scores by 40
+scores.artur += 40;
+
+// Increase Boris's scores by 50
+scores.boris += 50;
+
+// Show scores
+scores; // {artyom: 10, viktor: 35, egor: 33, artur: 50, boris: 55}
